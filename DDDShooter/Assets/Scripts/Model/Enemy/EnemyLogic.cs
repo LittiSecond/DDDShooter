b0f1fb49cfae21559565f5166069275cc8af7580
LittiSecond <breeze2882@yandex.ru>
2020-07-12 14:02:26 +0300

    Добавлен NPC, преследующий игрока используя NavMesh

---
+++ DDDShooter/Assets/Scripts/Model/Enemy/EnemyLogic.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Geekbrains;
+
+
+namespace DddShooter
+{
+    public sealed class EnemyLogic : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] private Transform _target;
+
+        private UnityEngine.AI.NavMeshAgent _agent;
+        private ITimeRemaining _timeRemaining;
+
+        private float _updateInterval = 0.5f;
+
+        #endregion
+
+
+        #region UnityMethods
+
+        private void Start()
+        {
+            _agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            _timeRemaining = new TimeRemaining(UpdateDestination, _updateInterval, true);
+            if (_target)
+            {
+                _timeRemaining.AddTimeRemaining();
+            }
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        private void UpdateDestination()
+        {
+            _agent.SetDestination(_target.position);
+        }
+
+        #endregion
+
+
+
+    }
+}
