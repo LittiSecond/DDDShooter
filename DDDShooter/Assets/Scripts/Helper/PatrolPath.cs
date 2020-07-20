1b539780440b00f31914169e32096ea0dd4eeba8
LittiSecond <breeze2882@yandex.ru>
2020-07-20 21:13:55 +0300

    Добавлено прорисовка пути патрулирования в редакторе

--- DDDShooter/Assets/Scripts/Helper/PatrolPath.cs
+++ DDDShooter/Assets/Scripts/Helper/PatrolPath.cs
@@ -8,7 +8,14 @@ namespace DddShooter
 {
     public sealed class PatrolPath : MonoBehaviour
     {
-        private List<Vector3> _path;
+        #region Fields
+
+        private List<Vector3> _path;// = new List<Vector3>();
+#if UNITY_EDITOR_WIN
+        private Color _lineColor = Color.red;
+#endif
+
+        #endregion
 
 
         #region UnityMethods
@@ -16,11 +23,7 @@ namespace DddShooter
         private void Awake()
         {
             _path = new List<Vector3>();
-            Transform[] transforms = GetComponentsInChildren<Transform>();
-            for (int i = 1; i < transforms.Length; i++)
-            {
-                _path.Add(transforms[i].position);
-            }
+            DeterminePath();
 
 #if UNITY_EDITOR_WIN
             string message = "PatrolPath->Awake: ";
@@ -32,6 +35,37 @@ namespace DddShooter
 #endif
         }
 
+#if UNITY_EDITOR_WIN
+
+        //private void OnValidate()
+        //{
+        //    DeterminePath();
+        //    Debug.Log("PatrolPath->OnValidate: " + gameObject.name);
+        //}
+
+        private void OnDrawGizmosSelected()
+        {
+            DeterminePath();
+            Gizmos.color = _lineColor;
+            for (var i = 0; i < _path.Count; i++)
+            {
+                Vector3 currentNode = _path[i];
+                Vector3 previousNode = Vector3.zero;
+                if (i > 0)
+                {
+                    previousNode = _path[i - 1];
+                }
+                else if (i == 0 && _path.Count > 1)
+                {
+                    previousNode = _path[_path.Count - 1];
+                }
+                Gizmos.DrawLine(previousNode, currentNode);
+                //Gizmos.DrawWireSphere(currentNode, 0.3f);
+                //Debug.Log("PatrolPath->OnDrawGizmosSelected: " + gameObject.name);
+            }
+        }
+#endif
+
         #endregion
 
 
@@ -40,6 +74,17 @@ namespace DddShooter
         public List<Vector3> GetPath()
         {
             return _path;
+        } 
+
+        private void DeterminePath()
+        {
+            _path.Clear();
+            Transform[] transforms = GetComponentsInChildren<Transform>();
+            for (int i = 1; i < transforms.Length; i++)
+            {
+                _path.Add(transforms[i].position);
+            }
+            //Debug.Log("PatrolPath->DeterminePath: _path.Count = " + _path.Count.ToString()); 
         }
 
         #endregion
